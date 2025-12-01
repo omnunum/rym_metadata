90b9b420d92dde73963e20401049ebda88101b1d
omnunum <3474285+omnunum@users.noreply.github.com>
2025-11-30 20:31:01 -0500

    Bump version to 1.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rym-metadata',
-    version='1.4.1',
+    version='1.4.2',
     description='RateYourMusic metadata scraper - standalone library and beets plugin',
     author='RYM Metadata',
     packages=find_packages(),
